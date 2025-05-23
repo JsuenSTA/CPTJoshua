8d9fcc13695a34e10054228b5882f3dd0cf9fe28
JsuenSTA <159478838+JsuenSTA@users.noreply.github.com>
2025-05-23 09:34:18 -0400

    testing

--- CPTJoshua.java
+++ CPTJoshua.java
@@ -3,6 +3,12 @@ import arc.*;
 public class CPTJoshua{
 	public static void main(String[] args){
 		Console con = new Console();
-			
+		int intx;
+		boolean blncheck = true;
+		intx = 0;
+		while (blncheck == true){
+			intx = intx+1;
+			con.println(intx);
+		}
 	}
 }
