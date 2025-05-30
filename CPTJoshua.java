46839553857c8e98eb0d6354e62f92e47a1fbbd1
JsuenSTA <159478838+JsuenSTA@users.noreply.github.com>
2025-05-30 09:42:29 -0400

    Changed the resolution and window title

--- CPTJoshua.java
+++ CPTJoshua.java
@@ -2,7 +2,7 @@ import arc.*;
 
 public class CPTJoshua{
 	public static void main(String[] args){
-		Console con = new Console();
+		Console con = new Console("Mathing training game", 1280,720);
 		String strname;
 		con.println("Enter your name: ");
 		strname = con.readLine();
