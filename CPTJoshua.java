aad5fde6088fd3a3c26fe827ff5b959899e8b3ed
JsuenSTA <159478838+JsuenSTA@users.noreply.github.com>
2025-06-02 09:43:42 -0400

    Attempting to do menu

--- CPTJoshua.java
+++ CPTJoshua.java
@@ -1,12 +1,28 @@
 import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.Font;
 
 public class CPTJoshua{
 	public static void main(String[] args){
 		Console con = new Console("Mathing training game", 1280,720);
+		//Varaibles
 		String strname;
-		con.println("Enter your name: ");
-		strname = con.readLine();
-		con.println("Welcome " +strname);
+		//Set backgroundcolor
 		
+		
+		//Menu
+		while(true){
+			con.drawString("Play (P)",500, 250);
+			con.drawString("LeaderBoard (L)" ,500,300);
+			con.drawString("Add quiz (A)" ,500,350);
+			con.drawString("Quit (Q)" ,500,400);
+			con.fillRect(0,0,1200,720);
+			char chrInput = con.getChar();
+			if(chrInput == 'P' || chrInput == 'p'){
+				con.setDrawColor(Color.BLACK);
+				con.fillRect(0,0,1200,720);
+			}
+		}
 	}
 }
