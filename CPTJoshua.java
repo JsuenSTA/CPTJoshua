ac84982fdc9f8a5642ee9bc23ca581e40d8572da
JsuenSTA <159478838+JsuenSTA@users.noreply.github.com>
2025-06-04 08:23:30 -0400

    changed some code and add master.txt

--- CPTJoshua.java
+++ CPTJoshua.java
@@ -6,12 +6,15 @@ import java.awt.Font;
 public class CPTJoshua{
 	public static void main(String[] args){
 		Console con = new Console("Mathing training game", 1280,720);
-		con.setDrawFont(new Font("Times New Roman", Font.BOLD,30));
+		//con.setDrawFont(new Font("Times New Roman", Font.BOLD,30));
 		//Varaibles
 		String strname;
 		//Set backgroundcolor
 		//Menu
 		while(true){
+			con.setDrawColor(Color.BLACK);
+			con.fillRect(0,0,1279,720);
+			con.setDrawColor(Color.WHITE);
 			con.drawString("Play (P)",500, 250);
 			con.drawString("LeaderBoard (L)" ,500,300);
 			con.drawString("Add quiz (A)" ,500,350);
@@ -23,14 +26,16 @@ public class CPTJoshua{
 					con.clear();
 					con.setDrawColor(Color.BLACK);
 					con.fillRect(0,0,1279,720);
-					System.out.println("work");
 					con.setDrawColor(Color.WHITE);
 					con.drawString("Username: ", 500, 250);
 					strname = con.readLine();
 					con.setDrawColor(Color.BLACK);
 					con.fillRect(0,0,1279,720);
+					con.clear();
 					con.setDrawColor(Color.WHITE);
-					con.drawString("Welcome" + strname, 500,250);
+					System.out.println(strname);
+					con.drawString("Select the quiz", 500, 250);
+					
 				}
 			}
 		}
