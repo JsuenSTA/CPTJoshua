8089d439ebb5a86dc196c71795a02bafa164637c
JsuenSTA <159478838+JsuenSTA@users.noreply.github.com>
2025-06-04 09:41:47 -0400

    wrote the method of counting questions

--- arraytest.java
+++ arraytest.java
@@ -5,6 +5,20 @@ import java.awt.Font;
 
 public class arraytest{
 	public static void main(String[] args){
-		
+		Console con = new Console();
+		String strQuiz[][];
+
 	}
+	public static int countQuestions(String strQuizfile){
+		TextInputFile txtQuizfile = new TextInputFile(strQuizfile);
+		int intLines = 0;
+		String strData;
+		while(txtQuizfile.eof() == false){
+			strData = txtQuizfile.readLine();
+			intLines++;
+		}
+		txtQuizfile.close();
+		return intLines/4;
+	}
+
 }
