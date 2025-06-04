0af57562bb014767f16292d919ee5ee15b46efff
JsuenSTA <159478838+JsuenSTA@users.noreply.github.com>
2025-06-04 11:16:01 -0400

    testing reading array

--- arraytest.java
+++ arraytest.java
@@ -7,8 +7,25 @@ public class arraytest{
 	public static void main(String[] args){
 		Console con = new Console();
 		String strQuiz[][];
-
+		int intCount = countQuestions("algebra.txt");
+		strQuiz = loadQuiz("algebra.txt",intCount);
 	}
+	public static String[][] loadQuiz(String strQuizfile, int intCount){
+		String strQuiz[][];
+		strQuiz = new String[intCount][4];
+		TextInputFile txtQuizfile = new TextInputFile(strQuizfile);
+		int intRow;
+		for(intRow = 0; intRow < intCount; intRow++){
+			strQuiz[intRow][0] = txtQuizfile.readLine();
+			strQuiz[intRow][1] = txtQuizfile.readLine();
+			strQuiz[intRow][2] = txtQuizfile.readLine();
+			strQuiz[intRow][3] = txtQuizfile.readLine();
+		}
+		txtQuizfile.close();
+		return strQuiz;
+	}
+
+	
 	public static int countQuestions(String strQuizfile){
 		TextInputFile txtQuizfile = new TextInputFile(strQuizfile);
 		int intLines = 0;
